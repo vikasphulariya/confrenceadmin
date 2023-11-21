88937bb1736d5a4d6fc23e31e448b023c4a4910f
vikas Phulriya <vikasPhulariya@gmail.com>
2023-11-21 16:11:38 +0530

    fix

--- pages/temp.jsx
+++ pages/temp.jsx
@@ -1,6 +1,25 @@
 "use client"
 import Image from "next/image";
-import { db } from "./firestoreConfig";
+// import { db } from "./firestoreConfig";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+const  firebaseConfig = {
+  apiKey: "AIzaSyAslvKIwGKp9m4YFbC-NZRbmmDXwBOfJmA",
+  authDomain: "confrencewebsite.firebaseapp.com",
+  projectId: "confrencewebsite",
+  storageBucket: "confrencewebsite.appspot.com",
+  messagingSenderId: "511860114926",
+  appId: "1:511860114926:web:45651c7ac1c8040406baff",
+  measurementId: "G-0J6GTCSWX4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+// export { db };
 import { Days_One, Inter } from "next/font/google";
 import {
   getDocs,
