64110ae328ea8bfb8cf588ae2dd076785f8eda34
vikas Phulriya <vikasPhulariya@gmail.com>
2023-12-06 10:58:22 +0530

    commit data

--- pages/pin.js
+++ pages/pin.js
@@ -1,4 +1,16 @@
- export  const PinData={
+ export const PinData={
     pin:6130
 
 } 
+import React from 'react';
+
+const PinPage = () => {
+    return (
+      <div>
+        {/* Your component logic here */}
+        <p>Pin: {PinData.pin}</p>
+      </div>
+    );
+  };
+  
+  export default PinPage;
