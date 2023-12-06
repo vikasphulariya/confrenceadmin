40ff3e44f7759d56d6c6da652a9d33677a4863b3
vikas Phulriya <vikasPhulariya@gmail.com>
2023-12-06 10:52:27 +0530

    Banner Work Complete

--- pages/Banner.js
+++ pages/Banner.js
@@ -1,11 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAslvKIwGKp9m4YFbC-NZRbmmDXwBOfJmA",
+  authDomain: "confrencewebsite.firebaseapp.com",
+  projectId: "confrencewebsite",
+  storageBucket: "confrencewebsite.appspot.com",
+  messagingSenderId: "511860114926",
+  appId: "1:511860114926:web:45651c7ac1c8040406baff",
+  measurementId: "G-0J6GTCSWX4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+export { db };
+import {
+  getDocs,
+  addDoc,
+  getDoc,
+  collection,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { useSelectedLayoutSegments } from "next/navigation";
 
 export default function Banner() {
+  const [dataUpdated, setDataUpdated] = useState(true);
+  useEffect(() => {
+    getData();
+  }, []);
+
+ 
+
   const [selectedImages, setSelectedImages] = useState([]);
-  const [imageUrls, setImageUrls] = useState([
-    "https://i.ibb.co/gMhbCz0/storm.png",
-  ]);
+  const [imageUrls, setImageUrls] = useState([]);
 
   const onFileChange = (event) => {
     const files = event.target.files;
@@ -16,7 +49,6 @@ export default function Banner() {
     setSelectedImages((prevImages) => [...prevImages, ...newImages]);
   };
 
-  
   const removeImage = (index) => {
     const updatedImages = [...selectedImages];
     updatedImages.splice(index, 1);
@@ -26,6 +58,7 @@ export default function Banner() {
     const updatedImagesurls = [...imageUrls];
     updatedImagesurls.splice(index, 1);
     setImageUrls(updatedImagesurls);
+    updateData(updatedImagesurls);
   };
   const uploadImagesToImgBB = async () => {
     try {
@@ -48,10 +81,40 @@ export default function Banner() {
       const newUrls = await Promise.all(uploadPromises);
 
       setImageUrls((prevUrls) => [...prevUrls, ...newUrls]);
-      setSelectedImages([])
+      let tempData=[...imageUrls,...newUrls]
+      updateData(tempData)
+      setSelectedImages([]);
     } catch (error) {
       console.error("Error uploading images:", error);
     }
+  };
+
+  const getData = async () => {
+    const firebaseData = doc(db, "Website", "Banner");
+
+    const docSnapshot = await getDoc(firebaseData);
+
+    if (docSnapshot.exists()) {
+      // Access the data from the document
+      const data = docSnapshot.data();
+      console.log("Document data:", data);
+      setImageUrls(data.Images);
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  const updateData = async (e) => {
+    // e.preventDefault();
+    const temp = doc(db, "Website", "Banner");
+    // console.log(dynamicData);
+    const k = { Images: e };
+    // Use the updateDoc function to update specific fields in the document
+    await updateDoc(temp, k).then(() => {
+      alert("Update successfull");
+    });
+
+    console.log("Document updated successfully");
   };
 
   return (
@@ -92,7 +155,7 @@ export default function Banner() {
         onClick={uploadImagesToImgBB}
         className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
       >
-        Upload Images to ImgBB
+        Upload Images 
       </button>
 
       <div className="mt-4">
